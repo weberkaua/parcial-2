1dfb0401648d8c57335c30c43d9afa8c54242104
weberkaua <weber.kaua@escola.pr.gov.br>
2026-03-18 08:47:25 -0300

    Update desafio 2.py

--- desafio 2.py
+++ desafio 2.py
@@ -1,1 +1,8 @@
+# Solicita um número ao usuário
+numero = int(input("Digite um número inteiro: "))
 
+# Verifica se o resto da divisão por 2 é zero
+if numero % 2 == 0:
+    print(f"O número {numero} é PAR.")
+else:
+    print(f"O número {numero} é ÍMPAR.")
