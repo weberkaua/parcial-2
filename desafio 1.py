b3b20e8506b9bc94c5b6b731be0781c3f154040e
weberkaua <weber.kaua@escola.pr.gov.br>
2026-03-18 08:36:54 -0300

    Update desafio 1.py

--- desafio 1.py
+++ desafio 1.py
@@ -1,1 +1,8 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
 
+# Calcula a soma
+soma = numero1 + numero2
+
+# Exibe o resultado
+print(f"A soma de {numero1} e {numero2} é {soma}.")
